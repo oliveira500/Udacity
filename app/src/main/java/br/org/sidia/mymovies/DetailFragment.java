613d36c852a4f0b1a0241a2f833f384bba3e99e6
Fabio Oliveira <fabio.f.oliveira500@gmail.com>
2018-07-16 20:47:16 -0400

    Partial development of the movie details screen

---
+++ app/src/main/java/br/org/sidia/mymovies/DetailFragment.java
@@ -0,0 +1,57 @@
+package br.org.sidia.mymovies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import br.org.sidia.mymovies.model.Movie;
+
+public class DetailFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String LOG = DetailFragment.class.getSimpleName();
+    private Movie mMovie;
+
+    public DetailFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+
+        Intent intent = getActivity().getIntent();
+
+        if (intent != null && intent.hasExtra(Movie.MOVIE_KEY)){
+            mMovie = intent.getParcelableExtra(Movie.MOVIE_KEY);
+        }
+
+        Log.v(LOG, mMovie.toString());
+
+        TextView detailTitle = (TextView) rootView.findViewById(R.id.detail_title);
+        TextView detailReleaseDate = (TextView) rootView.findViewById(R.id.detail_releaseDate);
+        TextView detailRating = (TextView) rootView.findViewById(R.id.detail_rating);
+        TextView detailOverview = (TextView) rootView.findViewById(R.id.detail_overview);
+        ImageView detailPoster = (ImageView) rootView.findViewById(R.id.detail_poster);
+
+        detailTitle.setText(mMovie.getOriginalTitle());
+        detailReleaseDate.setText(mMovie.getReleaseDate());
+        detailRating.setText(mMovie.getRating());
+        detailOverview.setText(mMovie.getOverview());
+        Picasso.get()
+                .load(mMovie.getPoster())
+                .placeholder(R.drawable.placeholder)
+                .into(detailPoster);
+        return rootView;
+    }
+}
