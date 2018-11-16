1476b1f7722eb458c3f233521efe01ce13de4442
Fabio Oliveira <fabio.f.oliveira500@gmail.com>
2018-11-16 19:43:50 -0400

    Development Project 04. In this project, you will use Gradle to create an application that tells jokes by dividing functionality into libraries and features to simplify development. You'll also set up a Google Cloud Endpoints development server to provide the jokes

---
+++ androidjoke/src/main/java/finalproject/sidia/com/androidjoke/JokeActivity.java
@@ -0,0 +1,34 @@
+package finalproject.sidia.com.androidjoke;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class JokeActivity extends AppCompatActivity {
+
+    public static String JOKE_KEY = "Joke key";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_joke, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
