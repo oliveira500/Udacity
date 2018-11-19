e075d3d76a1b3130eb1da79f3298f3bf5faf8e61
Fabio Oliveira <fabio.f.oliveira500@gmail.com>
2018-11-19 10:09:31 -0400

    Refactoring for send new version

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTestUnit.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTestUnit.java
@@ -4,17 +4,12 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
-import com.udacity.gradle.builditbigger.asynctask.EndpointsAsyncTask;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,7 +18,6 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class EndpointsAsyncTaskTestUnit{
-   String TAG = EndpointsAsyncTask.class.getSimpleName();
 
    @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
@@ -44,6 +38,5 @@ public class EndpointsAsyncTaskTestUnit{
         onView(withId(R.id.bt_action)).perform(click());
         onView(withId(R.id.joke_textview)).check(matches(isDisplayed()));
     }
-
 }
 
